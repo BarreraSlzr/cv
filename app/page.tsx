ffed99c9d694d699e39c93da6bc2fbb98d7bea0d
Emmanuel Barrera <barreraslzr@gmail.com>
2024-05-28 14:08:42 -0600

    Update CVData

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,32 @@ export default function Home(){
             name: 'Tariff Classifier for Aduanas',
             description: 'Created an website to ask for Aduana Classification having a text input to describe any kind of object.',
             skillsUsed: [2, 3, 4, 6, 12]
-          }
+          },
+          {
+            name: 'Audits and Jobs scheduler - 5 Renew Projects',
+            description: 'Refurbishment of 5 projects, each 5 years old since the last commit pushed, used to audit financial databases and manage recurring work schedules to monitor financial functionality with different currencies around the world.',
+            skillsUsed: [2, 3, 4, 7, 5]
+          },
+          {
+            name: 'Security Audits Dashboard',
+            description: 'Creation of a graphical dashboard platform to audit security compliance performance and facilitate accreditation and certification of business departments through manager role-based access.',
+            skillsUsed: [2, 3, 4, 5, 10, 22]
+          },
+          {
+            name: 'Chatbot Support',
+            description: 'Initialization and configuration of the support chatbot that is displayed on the company's website for end users to increase the company's engagement and support to customers and experienced users, specific information is queried in databases. Support ticket creation is also connected throughout a DevOps system.',
+            skillsUsed: [1, 7, 16, 23]
+          },
+          {
+            name: 'Many other small-projects as Fullstack or only Frontend capability',
+            description: 'I have created many other projects with less impact within the core business, such as internal laboratory management, the sensor network website with real-time information about the site, Stamping Monitor which was a website for real-time monitoring of Industry 4.0, or FlowChart that was a webapp where you can create a flow work and export it in csv to be used with a special font that creates flow-charts.',
+            skillsUsed: [1, 3, 4, 25]
+          },
+          {
+            name: 'Internal Collaboration with coworkers',
+            description: 'I was committed to increasing technology awareness among my co-workers with technical talks not only with other developers, but also with many non-technological people, such as the Human Resources Department, helping to automate their workflows using Microsoft's new capabilities.',
+            skillsUsed: [24, 25]
+          },
         ]
       },
       {
@@ -46,10 +71,10 @@ export default function Home(){
       { id: 4, name: 'Node.js', popularity: 70 },
       { id: 5, name: 'Next.js', popularity: 75 },
       { id: 6, name: 'CosmosDB', popularity: 65 },
-      { id: 7, name: 'HTML', popularity: 95 },
+      { id: 7, name: 'SQL Language - CTEs', popularity: 95 },
       { id: 8, name: 'SASS', popularity: 90 },
       { id: 9, name: 'Tailwind CSS', popularity: 80 },
-      { id: 10, name: 'Git', popularity: 85 },
+      { id: 10, name: 'SSO - Auth Providers', popularity: 85 },
       { id: 11, name: 'Docker', popularity: 70 },
       { id: 12, name: 'OpenAI', popularity: 60 },
       { id: 13, name: 'Azure Blob Storage', popularity: 70 },
@@ -60,7 +85,12 @@ export default function Home(){
       { id: 18, name: 'Videography', popularity: 65 },
       { id: 19, name: 'Social Media Marketing', popularity: 80 },
       { id: 20, name: 'Ticket Tech Support', popularity: 75 },
-      { id: 21, name: 'General Hardware Acknowledge', popularity: 70 }
+      { id: 21, name: 'General Hardware Acknowledge', popularity: 70 },
+      { id: 22, name: 'Power BI', popularity: 80 },
+      { id: 23, name: 'Cognigy', popularity: 60 },
+      { id: 24, name: 'Social Skills', popularity: 69 },
+      { id: 25, name: 'Embrace newest tech', popularity: 69 },
+      { id: 25, name: 'Tech Consultor', popularity: 72 },
     ],
     contactInfo: {
       email: 'BarreraSlzr@gmail.com',
