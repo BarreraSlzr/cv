a5cc6b764bb216a5bfdd01ec4171d8fdd84be6db
Emmanuel Barrera <barreraslzr@gmail.com>
2024-05-29 22:08:35 -0700

    Better icon representation index.tsx

--- components/forms/contact/index.tsx
+++ components/forms/contact/index.tsx
@@ -36,19 +36,16 @@ export default function ContactForm({ className, contact }: React.ComponentProps
         <h1 className="text-xl font-bold">{contact.fullname}</h1>
       </div>
       <p className="text-gray-700">{contact.bio}</p>
-      <div className="grid gap-2">
+      <div className="flex gap-4">
         <Link title="Send email" href={`mailto:${contact.email}`} className="w-6 h-6">
-          <FontAwesomeIcon icon={faEnvelope} className="text-blue-200" />
-          Send an email
+          <FontAwesomeIcon icon={faEnvelope} className="text-gray-200 hover:text-blue-400" />
         </Link>
         <Link title="Make a call" href={`tel:${contact.phone}`} className="w-6 h-6">
-          <FontAwesomeIcon icon={faPhone} className="text-blue-200" />
-          Call me
+          <FontAwesomeIcon icon={faPhone} className="text-gray-200 hover:text-blue-400" />
         </Link>
         {contact.urls.map((url, index) => (
           <Link title={url.title} key={index} href={url.url} className="w-6 h-6" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={iconMapping[url.icon]} className="text-blue-200" />
-            {url.title}
+            <FontAwesomeIcon icon={iconMapping[url.icon]} className="text-gray-200 hover:text-blue-400" />
           </Link>
         ))}
       </div>
