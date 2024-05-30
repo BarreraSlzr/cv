8de7c602da51f34ce9abbab9e6eb63e5e33cc534
Emmanuel Barrera <barreraslzr@gmail.com>
2024-05-29 22:29:41 -0600

    Update index.tsx

--- components/forms/contact/index.tsx
+++ components/forms/contact/index.tsx
@@ -35,14 +35,14 @@ export default function ContactForm({ className, contact }: React.ComponentProps
       </div>
       <p className="text-gray-700">{contact.bio}</p>
       <div className="flex justify-center">
-        <Link href={`mailto:${contact.email}`} className="mr-4">
+        <Link title="Send email" href={`mailto:${contact.email}`} className="mr-4">
           <FontAwesomeIcon icon={faEnvelope} className="text-blue-500" />
         </Link>
-        <Link href={`tel:${contact.phone}`} className="mr-4">
+        <Link title="Make a call" href={`tel:${contact.phone}`} className="mr-4">
           <FontAwesomeIcon icon={faPhone} className="text-blue-500" />
         </Link>
         {contact.urls.map((url, index) => (
-          <Link key={index} href={url.url} className="mr-4" target="_blank" rel="noopener noreferrer">
+          <Link title={url.title} key={index} href={url.url} className="mr-4" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={iconMapping[url.icon]} className="text-blue-500" />
           </Link>
         ))}
